e5f82b5235cdd8a579f7bd879fb568a9f262dbbc
Matheus Julidori <matheus_julidori@yahoo.com.br>
2023-09-11 20:31:45 -0300

    Texto e cabeçaho

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function () {
+  const sideMenuToggle = document.getElementById('side-menu');
+  const menuLinks = document.querySelectorAll('.menu a');
+
   
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 1) {
-        $('.arrow').addClass('fade');
-      } else{
-        $('.arrow').removeClass('fade');
-      }
-    })
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      sideMenuToggle.checked = false;
+      sideMenuToggle.style.display = none;
+    });
   });
-  
+});
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  const scrollY = window.scrollY;
+
+  if (scrollY > 0) {
+    header.style.top = "-100px"; /* Esconda o cabeçalho quando o usuário faz scroll para baixo */
+  } else {
+    header.style.top = "0"; /* Mostre o cabeçalho quando o usuário está no topo da página */
+  }
+});
