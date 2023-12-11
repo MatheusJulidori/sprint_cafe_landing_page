fa9854cb29337dde6f05d1b0f87f821864a7effd
Matheus Julidori <m.henrique@ges.inatel.br>
2023-12-11 15:21:49 -0300

    V2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const sideMenuToggle = document.getElementById('side-menu');
   const menuLinks = document.querySelectorAll('.menu a');
 
-  
+
   menuLinks.forEach(function (link) {
     link.addEventListener('click', function () {
       sideMenuToggle.checked = false;
@@ -21,3 +21,29 @@ window.addEventListener("scroll", function () {
     header.style.top = "0"; /* Mostre o cabeçalho quando o usuário está no topo da página */
   }
 });
+
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    center: true,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true, 
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+});
+
+
